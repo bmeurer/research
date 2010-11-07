0474938279bce71eedd74ad39ec830be81adc3a0
Benedikt Meurer <benedikt.meurer@googlemail.com>
2010-11-07 23:52:08 +0100

    Fix typo.

--- ocamljit2/ocamljit2-faster-objective-caml-benchmark/benchmark.c
+++ ocamljit2/ocamljit2-faster-objective-caml-benchmark/benchmark.c
@@ -303,7 +303,7 @@ int main(int argc, char **argv)
   printf("------------------------------------------------------------------------------\n");
   simple("almabench", 0, "number crunching");
   simple("almabench", 1, "number crunching {\\tiny(no bounds check)}");
-  simple("bdd", 0, "binary decision digram");
+  simple("bdd", 0, "binary decision diagram");
   simple("boyer", 0, "term processing");
   simple("fft", 0, "fast fourier transformation");
   simple("nucleic", 0, "floating point");
